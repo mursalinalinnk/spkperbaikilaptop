c283e79603b1c6b9f6d06db5a9041bbbc9bd3c3c
gensan <dinoseros19@gmail.com>
2015-07-07 23:02:12 +0700

    tugas ai using ubuntu

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include 'kepala.php'; ?>
+<div class="container">
+<div class="well well-small">
+
+  		<p>
+  		  <?php
+include("koneksi.php"); 
+echo "<CENTER><H1>Diagnosa Kerusakan pada rangkaian CPU</H1></CENTER>"; 
+echo "<CENTER><H1>Menggunakan Sistem Pakar</H1></CENTER>"; 
+echo "<CENTER><img src='CPU2.jpg' WIDTH='500' HEIGHT='500' BORDER=0 ALT=''></CENTER>"; 
+
+
+
+
+?>
+<a href='''' class='btn  btn-block btn-large ' /> <H1>MENU UTAMA</H1> 
+    </p>
+  		<p align="center"><a href="diagnosa_motherboard.php"><img src="Image Menu Utama/Motherboard.jpg" width="218" height="141" longdesc="Image Menu Utama/Motherboard.jpg" /></a> <a href="diagnosa_Hardisk.php"><img src="Image Menu Utama/Hard_disk.jpg" longdesc="Image Menu Utama/Hard_disk.jpg" /></a> <a href="diagnosa_vga.php"><img src="Image Menu Utama/vga.jpg" longdesc="Image Menu Utama/vga.jpg" /></a></p>
+  		<p align="center"><a href="diagnosa_powersupply.php"><img src="Image Menu Utama/powersupply.jpg" longdesc="Image Menu Utama/powersupply.jpg" /></a><a href="diagnosa_dvd.php"><img src="Image Menu Utama/DVD.jpg" longdesc="Image Menu Utama/DVD.jpg" /></a> <a href="diagnosa_RAM.php"><img src="Image Menu Utama/ram.jpg" longdesc="Image Menu Utama/ram.jpg" /></a></p>
+        
+       </br><div class=" well">
+<center><p>Indra Lesmana dan Unaesatul khasanah</p></center> </div>';
+
+</div>
+</div>
+
